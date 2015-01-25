3d55030b1ca75c6621e7d4cf6867810d4937e96f
asakura89 <flamboyan3no1a@gmail.com>
2015-01-25 19:55:06 +0700

    update version to v 1.1

--- KeyWielder/KeyWielder.Test/Properties/AssemblyInfo.cs
+++ KeyWielder/KeyWielder.Test/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KeyWielder.Test")]
+[assembly: AssemblyTitle("Keywielder.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ASMNetworkLab")]
-[assembly: AssemblyProduct("KeyWielder.Test")]
+[assembly: AssemblyProduct("Keywielder.Test")]
 [assembly: AssemblyCopyright("Copyright © ASMNetworkLab 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
--- KeyWielder/KeyWielder/Properties/AssemblyInfo.cs
+++ KeyWielder/KeyWielder/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("KeyWielder")]
+[assembly: AssemblyTitle("Keywielder")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ASMNetworkLab")]
-[assembly: AssemblyProduct("KeyWielder")]
+[assembly: AssemblyProduct("Keywielder")]
 [assembly: AssemblyCopyright("Copyright © ASMNetworkLab 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
