26db30c813ff7712fed081a03a3914de494cf9e1
asakura89 <flamboyan3no1a@gmail.com>
2015-01-25 11:09:53 +0700

    keywielder change to use fluent style

---
+++ KeyWielder/KeyWielder.Test/ExtendedCollection.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Ayumi
+{
+    public class ExtendedCollection
+    {
+        public delegate TResult Func<T, TResult>(T t);
+
+        public static Boolean Any<TSource>(IEnumerable<TSource> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            using (IEnumerator<TSource> e = source.GetEnumerator())
+                return e.MoveNext();
+        }
+
+        public static Boolean Any<TSource>(IEnumerable<TSource> source, Func<TSource, Boolean> predicate)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            return Any(Where(source, predicate));
+        }
+
+        public static Int32 Count<TSource>(IEnumerable<TSource> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            Int32 counter = 0;
+            using (IEnumerator<TSource> e = source.GetEnumerator())
+                while (e.MoveNext())
+                    counter++;
+
+            return counter;
+        }
+
+        public static Int32 Count<TSource>(IEnumerable<TSource> source, Func<TSource, Boolean> predicate)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            IEnumerable<TSource> predicateResult = Where(source, predicate);
+            return Count(predicateResult);
+        }
+
+        public static TSource FirstOrDefault<TSource>(IEnumerable<TSource> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            using (IEnumerator<TSource> e = source.GetEnumerator())
+                return e.MoveNext() ? e.Current : default(TSource);
+        }
+
+        public static TSource FirstOrDefault<TSource>(IEnumerable<TSource> source, Func<TSource, Boolean> predicate)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            return FirstOrDefault(Where(source, predicate));
+        }
+
+        public static IEnumerable<TSource> Where<TSource>(IEnumerable<TSource> source, Func<TSource, Boolean> predicate)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            using (IEnumerator<TSource> e = source.GetEnumerator())
+                while (e.MoveNext())
+                    if (predicate(e.Current) == true)
+                        yield return e.Current;
+        }
+
+        public static IEnumerable<TResult> Select<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, TResult> predicate)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            using (IEnumerator<TSource> e = source.GetEnumerator())
+                while (e.MoveNext())
+                    yield return predicate(e.Current);
+        }
+
+        public static Boolean Contains<TSource>(IEnumerable<TSource> source, TSource value)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            IEqualityComparer<TSource> comparer = EqualityComparer<TSource>.Default;
+            return Any(source, delegate(TSource item) { return comparer.Equals(item, value); });
+        }
+
+        public static IEnumerable<TSource> OrderBy<TSource, TKey>(IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            return OrderBy(source, keySelector, false);
+        }
+
+        public static IEnumerable<TSource> OrderBy<TSource, TKey>(IEnumerable<TSource> source, Func<TSource, TKey> keySelector, Boolean isDescending)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (keySelector == null) throw new ArgumentNullException("keySelector");
+
+            TSource[] sourceList = ToArray(source);
+            TKey[] keyList = new TKey[sourceList.Length];
+            for (int i = 0; i < sourceList.Length; i++)
+                keyList[i] = keySelector(sourceList[i]);
+
+            Int32 orderDirection = isDescending ? -1 : 1;
+            Comparer<TKey> keyComparer = Comparer<TKey>.Default;
+
+            for (int i = 0; i < sourceList.Length; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Int32 result = keyComparer.Compare(keyList[i], keyList[j]) * orderDirection;
+                    if (result < 0)
+                    {
+                        TSource temp = sourceList[i];
+                        sourceList[i] = sourceList[j];
+                        sourceList[j] = temp;
+
+                        TKey tempK = keyList[i];
+                        keyList[i] = keyList[j];
+                        keyList[j] = tempK;
+                    }
+                }
+            }
+
+            // to force array to IEnumerable
+            foreach (TSource s in sourceList)
+                yield return s;
+        }
+
+        public static List<TSource> ToList<TSource>(IEnumerable<TSource> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            return new List<TSource>(source);
+        }
+
+        public static TSource[] ToArray<TSource>(IEnumerable<TSource> source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            TSource[] arrayResult = new TSource[Count(source)];
+            Int32 idx = 0;
+            foreach (TSource item in source)
+            {
+                arrayResult[idx] = item;
+                idx++;
+            }
+
+            return arrayResult;
+        }
+
+        public static IEnumerable<TResult> Cast<TResult>(IEnumerable source)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+
+            foreach (Object current in source)
+                yield return (TResult)current;
+        }
+    }
+}
--- KeyWielder/KeyWielder.Test/KeyWielderTest.cs
+++ KeyWielder/KeyWielder.Test/KeyWielderTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using Ayumi;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace KeyWielder.Test
@@ -15,34 +16,34 @@ namespace KeyWielder.Test
         [TestInitialize]
         public void Initialize()
         {
-            KeyWielder.KeyWielderConfig
-                firstKeyPart1 = new KeyWielder.KeyWielderConfig { keyFormat = KeyWielder.KeyType.String, value = "GRE", valueLength = 3 },
-                firstKeyPart2 = new KeyWielder.KeyWielderConfig { keyFormat = KeyWielder.KeyType.Year, valueLength = 4 },
-                firstKeyPart3 = new KeyWielder.KeyWielderConfig { keyFormat = KeyWielder.KeyType.Month, valueLength = 2 },
-                firstKeyPart4 = new KeyWielder.KeyWielderConfig { keyFormat = KeyWielder.KeyType.Date, valueLength = 2 },
-                firstKeyPart5 = new KeyWielder.KeyWielderConfig { keyFormat = KeyWielder.KeyType.Counter, valueLength = 4, replacementChar = "0", currentCounterValue = 12 };
+            firstKey = KeyWielder
+                .New()
+                .AddString("GRE", 3)
+                .AddLongYear()
+                .AddNumericMonth()
+                .AddDate()
+                .AddCounter(12, 4)
+                .BuildKey();
 
-            var firstKeyConfigList = new List<KeyWielder.KeyWielderConfig> { firstKeyPart1, firstKeyPart2, firstKeyPart3, firstKeyPart4, firstKeyPart5 };
-            firstKey = KeyWielder.BuildKey(firstKeyConfigList);
             Debug.WriteLine(firstKey);
 
-            KeyWielder.KeyWielderConfig
-                secondKeyPart1 = new KeyWielder.KeyWielderConfig { keyFormat = KeyWielder.KeyType.String, value = "GRE", valueLength = 3, backSeparator = "/" },
-                secondKeyPart2 = new KeyWielder.KeyWielderConfig { keyFormat = KeyWielder.KeyType.Year, valueLength = 4, backSeparator = "-" },
-                secondKeyPart3 = new KeyWielder.KeyWielderConfig { keyFormat = KeyWielder.KeyType.Month, valueLength = 2, backSeparator = "-" },
-                secondKeyPart4 = new KeyWielder.KeyWielderConfig { keyFormat = KeyWielder.KeyType.Date, valueLength = 2, backSeparator = "/" },
-                secondKeyPart5 = new KeyWielder.KeyWielderConfig { keyFormat = KeyWielder.KeyType.Counter, valueLength = 4, replacementChar = "0", currentCounterValue = 237 };
+            secondKey = KeyWielder
+                .New()
+                .AddString("GRE", 3, "/")
+                .AddLongYear("-")
+                .AddNumericMonth("-")
+                .AddDate("/")
+                .AddCounter(237, 5)
+                .BuildKey();
 
-            var secondKeyConfigList = new List<KeyWielder.KeyWielderConfig> { secondKeyPart1, secondKeyPart2, secondKeyPart3, secondKeyPart4, secondKeyPart5 };
-            secondKey = KeyWielder.BuildKey(secondKeyConfigList);
             Debug.WriteLine(secondKey);
 
-            KeyWielder.KeyWielderConfig
-                thirdKeyPart1 = new KeyWielder.KeyWielderConfig { keyFormat = KeyWielder.KeyType.String, value = "ASAKURA", valueLength = 7 },
-                thirdKeyPart2 = new KeyWielder.KeyWielderConfig { keyFormat = KeyWielder.KeyType.Counter, valueLength = 2, replacementChar = "0", currentCounterValue = 77, counterIncrement = 12 };
+            thirdKey = KeyWielder
+                .New()
+                .AddString("ASAKURA", 7)
+                .AddCounter(77, 12, 2)
+                .BuildKey();
 
-            var thirdKeyConfigList = new List<KeyWielder.KeyWielderConfig> { thirdKeyPart1, thirdKeyPart2 };
-            thirdKey = KeyWielder.BuildKey(thirdKeyConfigList);
             Debug.WriteLine(thirdKey);
         }
 
@@ -54,8 +55,56 @@ namespace KeyWielder.Test
             String secondExpectation = currentDate.ToString("yyyy-MM-dd");
 
             Assert.AreEqual("GRE" + firstExpectation + "0013", firstKey); // change expectation to today's date
-            Assert.AreEqual("GRE/" + secondExpectation + "/0238", secondKey); // change expectation to today's date
+            Assert.AreEqual("GRE/" + secondExpectation + "/00238", secondKey); // change expectation to today's date
             Assert.AreEqual("ASAKURA89", thirdKey);
+        }
+
+        [TestMethod]
+        public void AllOutKeyTest()
+        {
+            var defaultMonthArray = new[] { String.Empty, "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+            var indonesianMonthArray = new[] { String.Empty, "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember" };
+            var indonesianMonthList = new List<String>(ExtendedCollection.Select(indonesianMonthArray, month => month.ToUpperInvariant()));
+            var indonesianDayArray = new[] { String.Empty, "Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu" };
+            var indonesianDayList = new List<String>(ExtendedCollection.Select(indonesianDayArray, day => day.ToUpperInvariant()));
+
+            DateTime currentDate = DateTime.Now;
+            Int32 currentDayOfWeek = Convert.ToInt32(currentDate.DayOfWeek) + 1;
+            Int32 currentMonth = currentDate.Month;
+            String currentMonthString = indonesianMonthArray[currentMonth];
+            String currentYearString = currentDate.Year.ToString();
+            String currentDayString = indonesianDayArray[currentDayOfWeek];
+            String separator = "•";
+            String fourthKey = KeyWielder
+                .New()
+                .AddString("DOC", 3)
+                .AddString("UMENTATION", 5, "/")
+                .AddShortYear("/")
+                .AddLongYear("/")
+                .AddShortMonth(separator)
+                .AddShortMonth(indonesianMonthArray, separator)
+                .AddLongMonth(separator)
+                .AddLongMonth(indonesianMonthList, separator)
+                .AddNumericMonth("/")
+                .AddDate("/")
+                .AddShortDay(separator)
+                .AddLongDay(separator)
+                .AddShortDay(indonesianDayArray, separator)
+                .AddLongDay(indonesianDayList, separator)
+                .AddNumericDay("/")
+                .AddCounter(123, 400, 5)
+                .BuildKey();
+
+            Debug.WriteLine(fourthKey);
+
+            Assert.AreEqual(
+                "DOCUMENT/" + currentYearString.Substring(2, 2) + "/" + currentYearString + "/" + currentMonthString.ToUpper().Substring(0, 3) +
+                separator + currentMonthString.Substring(0, 3) + separator +  defaultMonthArray[currentMonth].ToUpper() + separator +
+                currentMonthString.ToUpper() + separator + currentMonth.ToString().PadLeft(2, '0') + "/" + currentDate.Day.ToString().PadLeft(2, '0') + "/" +
+                currentDate.DayOfWeek.ToString().ToUpper().Substring(0, 3) + separator + currentDate.DayOfWeek.ToString().ToUpper() + separator +
+                currentDayString.Substring(0, 3) + separator + currentDayString.ToUpper() + separator + currentDayOfWeek.ToString().PadLeft(2, '0') +
+                "/00523",
+                fourthKey);
         }
 
         [TestMethod]
