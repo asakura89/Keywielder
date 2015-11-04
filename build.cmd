40a7487da31d5c8d747905658b12d6794319c785
alirasubratada <flamboyan3no1a@gmail.com>
2015-11-04 14:29:04 +0700

    update nuspec and add build script

---
+++ build.cmd
@@ -0,0 +1,58 @@
+@echo off
+
+set appname=Keywielder
+set config=Release
+set cwd=%CD%
+set outputdir=%cwd%\build
+set commonflags=/p:Configuration=%config%;AllowUnsafeBlocks=true /p:CLSCompliant=False
+
+set nugetversion=latest
+set cachednuget=%LocalAppData%\NuGet\nuget.%nugetversion%.exe
+
+if %PROCESSOR_ARCHITECTURE%==x86 (
+    set msbuild="%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe"
+) else (
+    set msbuild="%WINDIR%\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe"
+)
+goto build
+
+:build-error
+echo Failed to compile.
+
+:build
+echo ---------------------------------------------------------------------
+echo Building AnyCpu release...
+%msbuild% Keywielder.sln %commonflags% /tv:3.5 /p:TargetFrameworkVersion=v2.0 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\net20"
+if errorlevel 1 goto build-error
+%msbuild% Keywielder.sln %commonflags% /tv:3.5 /p:TargetFrameworkVersion=v3.5 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\net35"
+if errorlevel 1 goto build-error
+%msbuild% Keywielder.sln %commonflags% /tv:4.0 /p:TargetFrameworkVersion=v4.0 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\net40"
+if errorlevel 1 goto build-error
+%msbuild% Keywielder.sln %commonflags% /tv:4.0 /p:TargetFrameworkVersion=v4.5 /p:Platform="Any Cpu" /p:OutputPath="%outputdir%\net45"
+if errorlevel 1 goto build-error
+
+:done
+echo.
+echo ---------------------------------------------------------------------
+echo Compile finished.
+rem cd %cwd%
+rem goto exit
+
+if exist %cachednuget% goto copynuget
+echo Downloading latest version of NuGet.exe...
+if not exist %LocalAppData%\NuGet md %LocalAppData%\NuGet
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "$ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest 'https://dist.nuget.org/win-x86-commandline/%nugetversion%/nuget.exe' -OutFile '%cachednuget%'"
+goto createpackage
+
+:copynuget
+if exist .nuget\nuget.exe goto createpackage
+md .nuget
+copy %cachednuget% .nuget\nuget.exe > nul
+
+:createpackage
+echo Creating nuget package...
+.nuget\nuget.exe pack %appname%.nuspec -OutputDirectory "%outputdir%"
+cd %cwd%
+goto exit
+
+:exit
